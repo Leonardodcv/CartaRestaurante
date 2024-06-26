979e81b49d8093e1c41e22c1670a07d5307c9fc4
Leonardodcv <leodancarvel@gmail.com>
2024-06-25 23:08:27 -0600

    Verificacion de rutas, creacion de modelo de usuario  y modificaciones pequenas

--- icard_django/icard/icard/settings.py
+++ icard_django/icard/icard/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'drf_yasg',
-    'rest_framework',
+    "drf_yasg",
+    "rest_framework",
+
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#USERS
+AUTH_USER_MODEL = "users.User"
+
