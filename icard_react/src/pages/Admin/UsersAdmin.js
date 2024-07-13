6170f20274ae530b339743706b196c18fa83a174
Leonardodcv <leodancarvel@gmail.com>
2024-07-12 23:52:27 -0600

    creacion de modal basico para crear nuevo usurio y su formulario, validacion del formulario

--- icard_react/src/pages/Admin/UsersAdmin.js
+++ icard_react/src/pages/Admin/UsersAdmin.js
@@ -1,17 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Loader } from 'semantic-ui-react';
-import { HeaderPage, TableUsers } from '../../components/Admin';
+import { HeaderPage, TableUsers, AddEditUserForm } from '../../components/Admin';
 import { useUser } from '../../hooks';
+import { ModalBasic } from '../../components/Common';
 
 export  function UsersAdmin() {
+  const[showModal, setShowModal ] = useState(null)
+  const[titleModal, setTitleModal ] = useState(null)
+  const[contentModal, setContentModal ] = useState(null)
   const {loading, users, getUsers} = useUser();
-  //console.log(users);
 
   useEffect(()=>{getUsers()},[])
 
+  const openCloseModal = () => setShowModal((prev) => !prev);
+
+  const addUser =() =>{
+    setTitleModal("Nuevo usuario");
+    setContentModal(<AddEditUserForm/>)
+    openCloseModal();
+  }
+
   return (
     <>
-      <HeaderPage title="Usuarios" btnTitle="Nuevo Usuario"/>
+      <HeaderPage 
+        title="Usuarios" 
+        btnTitle="Nuevo Usuario" 
+        btnClick={addUser}
+      />
       { loading ? (
         <Loader active inline="centered">
           Cargando...
@@ -19,6 +34,12 @@ export  function UsersAdmin() {
       ):(
         <TableUsers users={users}/>
       )}
+      <ModalBasic 
+        show = {showModal} 
+        onClose={openCloseModal} 
+        title={titleModal} 
+        children={contentModal} 
+        />
     </>
   )
 }
