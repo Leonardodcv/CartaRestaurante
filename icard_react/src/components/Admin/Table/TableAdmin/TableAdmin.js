e0f3055b6523d67d6fbdb0897e78dab9024712db
Leonardodcv <leodancarvel@gmail.com>
2024-09-19 23:00:54 -0600

    Recarga manual y automatica de la pagina web

--- icard_react/src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ icard_react/src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -9,10 +9,10 @@ import { ReactComponent as IcTable} from "../../../../assets/mesa.svg";
 import "./TableAdmin.scss";
 
 export  function TableAdmin(props) {
-  const {table} = props;
+  const { table, reload } = props;
   const [ orders, setOrders ] = useState([]);
   const [ tableBusy, setTableBusy] = useState(false)
-  console.log(orders);
+  //console.log(orders);
 
   
   useEffect(() => {
@@ -27,7 +27,8 @@ export  function TableAdmin(props) {
         console.error("Error fetching orders:", error);
       }
     })();
-  }, [table.id]);
+  }, [reload]);
+  //}, [table.id]);
   
   useEffect(() => {
     (async () => {
@@ -42,7 +43,8 @@ export  function TableAdmin(props) {
         console.error("Error fetching orders:", error);
       }
     })();
-  }, [table.id]);
+  }, [reload]);
+  //}, [table.id]);
     
 
   return (
@@ -58,3 +60,66 @@ export  function TableAdmin(props) {
     </Link>
   );
 }
+
+/*import React, { useState, useEffect} from 'react';
+import { initial, size } from 'lodash';
+import { Label, Button, Checkbox, Icon } from 'semantic-ui-react';
+import classNames from "classnames";
+import { Link } from 'react-router-dom';
+import { getOrderByTableApi } from '../../../../api/orders';
+import { ORDER_STATUS } from '../../../../utils/constans';
+import { ReactComponent as IcTable} from "../../../../assets/mesa.svg";
+import "./TableAdmin.scss";
+
+export  function TableAdmin(props) {
+  const { table, reload } = props;
+  const [ orders, setOrders ] = useState([]);
+  const [ tableBusy, setTableBusy] = useState(false)
+  //console.log(orders);
+
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getOrderByTableApi(
+          table.id,
+          ORDER_STATUS.PENDING
+        );
+        setOrders(response);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    })();
+  }, [reload]);
+  //}, [table.id]);
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getOrderByTableApi(
+          table.id,
+          ORDER_STATUS.DELIVERED
+        );
+        if(size(response)>0) setTableBusy(response);
+        else setTableBusy(false)
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    })();
+  }, [reload]);
+  //}, [table.id]);
+    
+
+  return (
+    <Link className='table-admin' to={`/admin/table/${table.id}`}>
+      {size(orders) > 0 ? <Label circular color='orange'>{size(orders)}</Label> : null}
+      <IcTable 
+        className= { classNames({
+          pending:size(orders) >0,
+          busy: tableBusy,
+        })}
+      />
+      <p>Mesa {table.number}</p>
+    </Link>
+  );
+}*/
--- icard_react/src/components/Admin/Table/TableListAdmin/TableListAdmin.js
+++ icard_react/src/components/Admin/Table/TableListAdmin/TableListAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Label, Button, Checkbox, Icon } from 'semantic-ui-react';
 import { map, size } from 'lodash';
 import { TableAdmin } from '../TableAdmin';
@@ -6,24 +6,53 @@ import "./TableListAdmin.scss"
 
 export  function TableListAdmin(props) {
     const {tables} = props;
+    const [ reload, setReload] = useState(false);
+    const [ autoReload, setAutoReload ] = useState(false);
+
+    const onReload = () => setReload((prev) => !prev);
+
+    useEffect(()=>{
+        if(autoReload) {
+            const autoReloadAction = () => {
+                onReload();
+                setTimeout(() => {
+                    autoReloadAction();
+                }, 5000);
+            };
+            autoReloadAction();
+        }
+    }, [autoReload]);
+
+
+    const onCheckAutoReload = (check) => {
+        if(check){
+            setAutoReload(check);
+        } else {
+            window.location.reload();
+        }
+    }
+
   return (
     <div className='tables-list-admin'>
         <Button 
             primary 
             icon 
             className='tables-list-admin__reload' 
-            onClick={() => console.log("onRefetchReload")}
+            onClick={onReload}
         >
             <Icon name='refresh' />
         </Button> 
 
         <div className='tables-list-admin__reload-toggle'>
             <span>Reload automatico</span>
-            <Checkbox toggle onChange={(_, data) => console.log(data.checked)} />
+            <Checkbox 
+                toggle
+                checked = { autoReload }
+                onChange = {(_, data) => onCheckAutoReload(data.checked)} />
         </div>
 
         {map (tables, (table) => (
-            < TableAdmin key = {table.number} table ={table}/>
+            < TableAdmin key = {table.number} table ={table} reload = {reload}/>
         ))}
     </div>
   )
